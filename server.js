a9c441f390457c2e0e743cf0245369c54d66ad17
Vova Kulikov <v.kulikov@andersenlab.com>
2017-08-23 23:52:46 +0300

    Add route for static

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use('/', express.static(__dirname + '/'));
 app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
